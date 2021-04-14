7e3734179ddb10fc9096c2b1a05d7335153512f7
Raju Ahmed Shetu <shetu2153@gmail.com>
2021-04-15 01:21:58 +0600

    updated the code base

--- scripts/sms_scripts.go
+++ scripts/sms_scripts.go
@@ -34,10 +34,7 @@ func TestSMSSending() {
 	to := "8801886267494"
 	log.Println(apiKey, to, englishString)
 	log.Println(service)
-	//model, err := service.SendTextMessage(apiKey, englishString, to, "en", "bd", true)
-	model, err := service.SendTextMessage(apiKey, "Sending the message using go microservice. Reply me if you get it.", "8801833181961", "en", "bd", false)
-	//model, err := service.SendTextMessage(apiKey, "Sending the message using go microservice. Reply me if you get it.", "8801886267494", "en", "bd", false)
-
+	model, err := service.SendTextMessage(apiKey, englishString, to, "en", "bd", true)
 	if err != nil {
 		log.Println(err.Error())
 		return
